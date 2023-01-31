3d3a9070946f4689189b49d9faa8a66e980341ca
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2023-01-31 07:44:49 -0300

    Adicionando ApiPlatform

--- rector.php
+++ rector.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Symfony\Set\SensiolabsSetList;
+use Rector\Symfony\Set\SymfonySetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -19,4 +22,10 @@ return static function (RectorConfig $rectorConfig): void {
     //    $rectorConfig->sets([
     //        LevelSetList::UP_TO_PHP_81
     //    ]);
+
+    $rectorConfig->sets([
+        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
+        SensiolabsSetList::FRAMEWORK_EXTRA_61,
+    ]);
 };
